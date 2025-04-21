672b61d46d986b1d7a9ea3bc1b76a1633a2129cc
Andre Lucas <andre.lucas@tmlmobilidade.pt>
2025-04-21 04:13:39 +0100

    skeleton - ui

---
+++ app/components/Sections.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps<{
+  subtitle: string
+  title: string
+}>()
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h4 class="title">
+            <span class="theme-gradient">
+              {{subtitle}}
+            </span>
+          </h4>
+          <h6 class="title mb--30">
+            {{title}}
+          </h6>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
